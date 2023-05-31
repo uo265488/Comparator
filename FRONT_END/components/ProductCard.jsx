b851d9d8aca4f4f190126066486dd74215d61e18
uo265488 <uo265488@uniovi.es>
2023-05-31 19:35:48 +0200

    Transport of FRONT-END due to issues with repos (main != master)

---
+++ FRONT_END/components/ProductCard.jsx
@@ -0,0 +1,61 @@
+import { CardActions, CardContent, CardMedia, Typography} from "@mui/material";
+import { Button, Card } from "react-native-paper";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { añadirProductoALaLista } from "../redux/reduxConfig";
+import { StyledButton } from "./ProductInformation";
+import { productoAElemento } from "../helper/parser";
+
+
+
+export default function ProductCard(props) {
+  
+  const dispatch = useDispatch();
+
+  const onAddToLaLista = (clickedItem) => {
+    dispatch(añadirProductoALaLista(clickedItem, { payload : clickedItem.producto.barcode }));
+  }
+
+  let imageRef = require("../static/images/producto.png");
+  const navigate = useNavigate();
+    return (
+      <Card sx={{ width: { md: 345, xs: 250 }, height: { md: 500, xs: 350 }, bgcolor: "background.card", borderRadius: 8, boxShadow: '10' }}>
+        <CardMedia
+          component="img"
+          sx={{ height: { md: 300, xs: 150 } }}
+          image={imageRef}
+          alt={props.product.nombre}
+        />
+        <CardContent sx={{ height: { xs: 100, md: 120 } }}>
+          <Typography gutterBottom variant="h5" component="div" sx={{ typography: { md: 'h5', xs: 'h6' } }}>
+            {props.product.nombre}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{ typography: { md: 'h5', xs: 'h6' } }}>
+            {parseFloat(props.product.precios[props.product.precios.length - 1]).toFixed(2)} €
+          </Typography>
+          <Typography variant="body2" color="text.secondary" >
+            Supermercado: {props.product.supermercado}
+                </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Marca: {props.product.marca}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Proveedor: {props.product.proveedor}
+          </Typography>
+        </CardContent>
+        <CardActions >
+          {//<Button size="small">Share</Button>
+          }
+          <StyledButton sx={{ bgcolor: 'background.button', ":hover": { bgcolor: 'background.buttonhover' }, color: 'text.dark', ml: { xs: 0, md: 6 } }}
+            onPress={_event => onAddToLaLista(productoAElemento(props.product))} mode="contained"
+          >
+            Añadir a la lista de la compra
+          </StyledButton>
+          <Button sx={{ bgcolor: 'background.button', ":hover": { bgcolor: 'background.buttonhover' }, color: 'text.dark' }}
+            onClick={_event => navigate("/productos/${props.producto.barcode}")}>
+            Ver más
+          </Button>
+        </CardActions>
+      </Card>
+    )
+}
