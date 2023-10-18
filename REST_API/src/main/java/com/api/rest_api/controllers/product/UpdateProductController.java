21d7b7c58996307359ff4cddbd801514b4012a76
uo265488 <uo265488@uniovi.es>
2023-10-18 12:59:45 +0200

    Create UpdateProductController.java

---
+++ REST_API/src/main/java/com/api/rest_api/controllers/product/UpdateProductController.java
@@ -0,0 +1,22 @@
+package com.api.rest_api.controllers.product;
+
+import co.elastic.clients.elasticsearch.core.UpdateResponse;
+import com.api.rest_api.documents.Product;
+import com.api.rest_api.services.product.ProductUpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/update")
+public class UpdateProductController {
+
+    @Autowired
+    private ProductUpdateService updateService;
+
+    @CrossOrigin(origins = "*")
+    @PutMapping("product")
+    public ResponseEntity<UpdateResponse<Product>> update(@RequestBody Product product) {
+        return ResponseEntity.ok(updateService.updateDocument(product));
+    }
+}
