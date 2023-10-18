c73068b9641b9c5c4d684904c4d17d25da5014ae
uo265488 <uo265488@uniovi.es>
2023-10-18 13:01:22 +0200

    Create ListaSearchService.java

---
+++ REST_API/src/main/java/com/api/rest_api/services/lista/ListaSearchService.java
@@ -0,0 +1,20 @@
+package com.api.rest_api.services.lista;
+
+import com.api.rest_api.documents.ListaResponseModel;
+import com.api.rest_api.repositories.search.ListaSearchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ListaSearchService {
+
+    @Autowired
+    private ListaSearchRepository repo;
+
+    public ListaResponseModel getListasByAutor(String autor) {
+        //exists autor
+
+        return new ListaResponseModel(repo.filterByFieldQuery("autor", autor));
+    }
+
+}
