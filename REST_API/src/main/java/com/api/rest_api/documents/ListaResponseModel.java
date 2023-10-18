c888de3754cd4fcf11d73608e6a728e6f1471226
uo265488 <uo265488@uniovi.es>
2023-10-18 13:00:38 +0200

    ResponseModel for Lista

---
+++ REST_API/src/main/java/com/api/rest_api/documents/ListaResponseModel.java
@@ -0,0 +1,37 @@
+package com.api.rest_api.documents;
+
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import com.api.rest_api.helper.parser.ListaParser;
+import lombok.Value;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Value
+public class ListaResponseModel {
+
+    List<Lista> hits = new ArrayList<>();
+
+    public ListaResponseModel(SearchResponse<Lista> response) {
+        addHits(response);
+    }
+    /**
+     * Adds the List to the hits
+     * @param  response
+     */
+    public void addHits(SearchResponse<Lista> response) {
+        response.hits().hits().forEach(h ->
+        {
+            if(!this.hits.contains(h)) {
+                hits.add(ListaParser.mapToLista(h.source()));
+            }
+        });
+    }
+    @Override
+    public String toString() {
+        if(hits.isEmpty())
+            return "No product found";
+        return hits.stream().map(h -> h.toString()).reduce((h, h1) -> h + " " + h1).get();
+    }
+
+}
