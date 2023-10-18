1b2591592cad1c8183d7141772b173bd8efde9de
uo265488 <uo265488@uniovi.es>
2023-10-18 12:59:38 +0200

    Create ImageController.java

---
+++ REST_API/src/main/java/com/api/rest_api/controllers/images/ImageController.java
@@ -0,0 +1,50 @@
+package com.api.rest_api.controllers.images;
+
+import com.api.rest_api.documents.ImageRequest;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Base64;
+
+@RestController
+public class ImageController {
+
+    @Value("C:\\Users\\oscar\\OneDrive\\Desktop\\TFG\\project\\REST_API\\src\\main\\resources\\")
+    private String saveLocation;
+
+    @PostMapping("/utils/saveImage")
+    public ResponseEntity<String> saveImage(@RequestBody ImageRequest imageRequest) {
+        String base64Image = imageRequest.getBase64Image();
+        String barcode = imageRequest.getBarcode();
+
+        // Remove the data URI scheme part
+        String base64Data = base64Image.replaceFirst("data:image\\/[^;]+;base64,", "");
+
+        // Decode the base64 data
+        byte[] imageBytes = Base64.getDecoder().decode(base64Data);
+
+        // Generate a unique filename or use an existing filename
+        String filename = barcode + ".png";
+
+        // Specify the target file path within the project folder
+        String targetPath = saveLocation + filename;
+
+        try {
+            // Save the image file to the specified location
+            FileUtils.writeByteArrayToFile(new File(targetPath), imageBytes);
+            System.out.println("Image saved successfully.");
+            return ResponseEntity.ok("Image saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Error occurred while saving the image: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while saving the image.");
+        }
+    }
+}
+
