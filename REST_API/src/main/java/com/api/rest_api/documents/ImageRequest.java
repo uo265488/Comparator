a9d86c27466a4ef83baff4436654978dd28b0c79
uo265488 <uo265488@uniovi.es>
2023-10-18 12:59:51 +0200

    Create ImageRequest.java

---
+++ REST_API/src/main/java/com/api/rest_api/documents/ImageRequest.java
@@ -0,0 +1,14 @@
+package com.api.rest_api.documents;
+
+public class ImageRequest {
+    public String barcode;
+    public String base64Image;
+
+    public String getBarcode() {
+        return barcode;
+    }
+    public String getBase64Image() {
+        return base64Image;
+    }
+}
+
