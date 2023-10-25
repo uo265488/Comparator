f0ef3878eec9d121d710f6763ff960d87629a416
uo265488 <uo265488@uniovi.es>
2023-10-25 13:00:49 +0200

    Update App.js

--- FRONT_END/App.js
+++ FRONT_END/App.js
@@ -10,17 +10,9 @@ import Catalogo from './routes/Catalogo';
 import LaLista from './routes/LaLista';
 import DashboardContent from './routes/Dashboard';
 import { saveState } from './redux/localStorage';
-import { Auth0Provider } from '@auth0/auth0-react';
-import Login from './routes/Login';
-import Profile from './components/authentication/Profile';
+import Profile from './routes/Profile';
 
 export default function App() {
-
-  {/*store.subscribe(throttle(() => {
-    saveState({
-      laLista: store.getState()
-    });
-  }, 1000));*/}
 
   store.subscribe(() => {
     saveState(store.getState());
@@ -39,7 +31,6 @@ export default function App() {
               <Route path="lista" element={<LaLista></LaLista>}></Route>
               <Route path="estadisticas" element={<DashboardContent></DashboardContent>}></Route>
               <Route path="profile" element={<Profile></Profile>}></Route>
-              
             </Routes>
           </BrowserRouter>
         </Provider>
