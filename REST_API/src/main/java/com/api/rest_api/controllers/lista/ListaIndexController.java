e26e4d41333f18a8a4726275427a0f66989a1b42
uo265488 <uo265488@uniovi.es>
2024-02-24 09:53:25 +0100

    [LaLista functionality] Create indices fixed

--- REST_API/src/main/java/com/api/rest_api/controllers/lista/ListaIndexController.java
+++ REST_API/src/main/java/com/api/rest_api/controllers/lista/ListaIndexController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,7 @@ public class ListaIndexController {
     @Autowired
     private LaListaProductIndexService laListaProductIndexService;
 
-    @PostMapping("/createIndex")
+    @GetMapping("/createIndex")
     public ResponseEntity createIndices() {
 
         return listaIndexService.createIndex() && laListaProductIndexService.createIndex()
--- REST_API/src/main/java/com/api/rest_api/helper/Indices.java
+++ REST_API/src/main/java/com/api/rest_api/helper/Indices.java
@@ -3,5 +3,5 @@ package com.api.rest_api.helper;
 public class Indices {
     public static final String PRODUCT_INDEX = "products";
     public static final String LISTAS_INDEX = "listas";
-    public static final String LALISTA_PRODUCT_INDEX = "laListaProducts";
+    public static final String LALISTA_PRODUCT_INDEX = "listaproducts";
 }
