e0326433e55be5e155bc2b853e6a9ee2d3918904
uo265488 <uo265488@uniovi.es>
2024-04-08 16:56:43 +0200

    [BACKEND] StatisticsController.java refactored and solved endpoint standarization

--- REST_API/src/main/java/com/api/rest_api/controllers/statistics/StatisticsController.java
+++ REST_API/src/main/java/com/api/rest_api/controllers/statistics/StatisticsController.java
@@ -2,6 +2,7 @@ package com.api.rest_api.controllers.statistics;
 
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import com.api.rest_api.documents.Product;
+import com.api.rest_api.documents.responseModels.ProductResponseModel;
 import com.api.rest_api.services.product.ProductSearchService;
 import com.google.gson.Gson;
 import org.slf4j.Logger;
@@ -11,17 +12,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/statistics")
+@RequestMapping("/api/v1/statistics")
 public class StatisticsController {
     @Autowired
-    private ProductSearchService service;
+    private ProductSearchService productSearchService;
 
     private static final Logger logger = LoggerFactory.getLogger("Logger");
+
+    @GetMapping("/mostUpdated")
+    public ResponseEntity<ProductResponseModel> getMostUpdatedProduct() {
+
+        ProductResponseModel res = productSearchService.getMostUpdated();
+        logger.info(res.toString());
+
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/lastPriceChange")
+    public ResponseEntity<ProductResponseModel> getMostRecentUpdate() {
+
+        return ResponseEntity.ok(productSearchService.mostRecentUpdate());
+    }
 
     @GetMapping("/avgPriceBySupermercado")
     public ResponseEntity<Object> getAvgPriceBySupermercado() {
 
-        SearchResponse<Product> response = service.getAveragePricesBySupermercado();
+        SearchResponse<Product> response = productSearchService.getAveragePricesBySupermercado();
 
         Gson gson = new Gson();
         String json = gson.toJson(response.aggregations());
@@ -31,7 +47,7 @@ public class StatisticsController {
 
     @GetMapping("/avgPricePerMonthBySupermercado")
     public ResponseEntity<Object> getAvgPricePerMonthBySupermercado() {
-        return ResponseEntity.ok(service.getAvgPricePerMonthBySupermercado());
+        return ResponseEntity.ok(productSearchService.getAvgPricePerMonthBySupermercado());
     }
 
 }
