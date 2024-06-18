c130a16a04d62bfa8dcb0a9466b2e720c0d99929
uo265488 <uo265488@uniovi.es>
2024-06-18 14:53:36 +0200

    New Utils method

--- FRONT_END/helper/utils.js
+++ FRONT_END/helper/utils.js
@@ -1,3 +1,7 @@
 export default function isNullOrEmpty(str) {
     return (str ?? '') === '';
 }
+
+export function isArrayNullOrUndefined(array) {
+    return array == null || array == undefined;
+}
