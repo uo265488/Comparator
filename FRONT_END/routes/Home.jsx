f9237b6e9143e5274266dac5aab7c847d178d8a6
uo265488 <uo265488@uniovi.es>
2024-07-08 15:07:45 +0200

    [FRONTEND] Home Page

--- FRONT_END/routes/Home.jsx
+++ FRONT_END/routes/Home.jsx
@@ -1,38 +1,76 @@
-
-import { Grid } from "@mui/material";
-import { Button, Text } from "react-native-paper";
+import { Grid, Typography, Button, Box } from "@mui/material";
 import { Link } from "react-router-dom";
+import backgroundImage from '../static/images/home.jpeg'; 
 
 export default function Home() {
-
-  
   return (
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      direction="column"
+    <Box
       sx={{
-        bgcolor: "background.default",
-        height: "93vh",
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "white",
+        position: "relative",
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Text> Bienvenido a mi aplicación!!</Text>
-      <Link to="/scanner" underline="none">
-        <Button mode="contained">Scannear producto</Button>
-      </Link>
-      <Link to="/productos" underline="none">
-        <Button mode="contained">Hacer una lista de la compra</Button>
-      </Link>
-      <Link to="/laLista" underline="none">
-        <Button mode="contained">Crea LaLista</Button>
-      </Link>
-      <Link to="/estadisticas" underline="none">
-        <Button mode="contained">Ver estadísticas</Button>
-      </Link>
-      <Link to="/tusListas" underline="none">
-        <Button mode="contained">Tus listas</Button>
-      </Link>
-    </Grid>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          opacity: 0.1,
+          zIndex: 1,
+        }}
+      />
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+        sx={{
+          zIndex: 2,
+          color: "black",
+          textAlign: "center",
+          padding: 2,
+        }}
+      >
+        <Typography variant="h3" gutterBottom>
+          ¡Bienvenido a Comparator!
+        </Typography>
+        <Link to="/scanner" style={{ textDecoration: 'none', margin: 8 }}>
+          <Button variant="contained" sx={{ bgcolor: "rgb(97, 75, 154)" }} size="large">
+            Scanner de productos
+          </Button>
+        </Link>
+        <Link to="/productos" style={{ textDecoration: 'none', margin: 8 }}>
+          <Button variant="contained" sx={{ bgcolor: "rgb(97, 75, 154)" }} size="large">
+            Catálogo de productos
+          </Button>
+        </Link>
+        <Link to="/lista" style={{ textDecoration: 'none', margin: 8 }}>
+          <Button variant="contained" sx={{ bgcolor: "rgb(97, 75, 154)" }} size="large">
+            Mejora tu Lista de Productos
+          </Button>
+        </Link>
+        <Link to="/listasPersonales" style={{ textDecoration: 'none', margin: 8 }}>
+          <Button variant="contained" sx={{ bgcolor: "rgb(97, 75, 154)" }} size="large">
+            Tus listas
+          </Button>
+        </Link>
+        <Link to="/estadisticas" style={{ textDecoration: 'none', margin: 8 }}>
+          <Button variant="contained" sx={{ bgcolor: "rgb(97, 75, 154)" }} size="large">
+            Ver estadísticas
+          </Button>
+        </Link>
+      </Grid>
+    </Box>
   );
 }
